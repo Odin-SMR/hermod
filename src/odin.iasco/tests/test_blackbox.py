6e233bcea6df83136067cdc6bb728079cc3956a2
marjan <marjan@39c22ef0-5628-0410-b279-a288a0259246>
2010-04-20 14:37:23 +0000

    Adding test-script for blackbox
    git-svn-id: http://svn.rss.chalmers.se/svn/odinsmr/hermod/trunk@1029 39c22ef0-5628-0410-b279-a288a0259246

---
+++ src/odin.iasco/tests/test_blackbox.py
@@ -0,0 +1,30 @@
+import unittest
+import mocker
+from odin.iasco.blackbox_main import main
+
+def getexcept(e):
+    raise e
+
+class BlackboxTestCase(unittest.TestCase):
+    def setUp(self):
+        self.main = main()
+
+    def noDates(self):
+	main = self.mocker.mock()
+	main.getNewDates()
+	self.mocker.result([])
+	main.getStartDate()
+	self.mocker.result([])
+        
+
+
+def test_suite():
+    tests = [
+            'config',
+            'logger',
+            ]
+    return unittest.TestSuite(map(BlackboxTestCase,tests))
+
+if __name__=='__main__':
+    unittest.TextTestRunner(verbosity=3).run(test_suite())
+

a0fe319c4017ad7d364b050940537b21fdc0c324
joakim <joakim@39c22ef0-5628-0410-b279-a288a0259246>
2010-04-20 11:43:07 +0000

    added environment tests
    git-svn-id: http://svn.rss.chalmers.se/svn/odinsmr/hermod/trunk@1021 39c22ef0-5628-0410-b279-a288a0259246

---
+++ src/odin.config/tests/test_environment.py
@@ -0,0 +1,40 @@
+import unittest
+from odin.config.environment import config,HermodError,HermodWarning
+import logging
+import logging.config
+from pkg_resources import resource_stream
+
+def getexcept(e):
+    raise e
+
+class EnvironmentTestCase(unittest.TestCase):
+    def setUp(self):
+        self.config = config()
+
+    def config(self):
+        sections = self.config.sections()
+        testsection = ['logging']
+        for section in testsection:
+            self.assertTrue(section in sections)
+
+    def HermodError(self):
+        self.assertRaises(HermodError,getexcept,HermodError)
+
+    def logger(self):
+        name=self.config.get('logging','configfile')
+        file = resource_stream(__name__,name)
+        logging.config.fileConfig(file)
+
+        
+
+
+def test_suite():
+    tests = [
+            'config',
+            'logger',
+            ]
+    return unittest.TestSuite(map(EnvironmentTestCase,tests))
+
+if __name__=='__main__':
+    unittest.TextTestRunner(verbosity=3).run(test_suite())
+

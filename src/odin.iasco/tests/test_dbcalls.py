97e75c5ae2181acea8483f85cc06ae9bf47d389d
marjan <marjan@39c22ef0-5628-0410-b279-a288a0259246>
2010-04-30 07:45:30 +0000

    Added new tests
    git-svn-id: http://svn.rss.chalmers.se/svn/odinsmr/hermod/trunk@1051 39c22ef0-5628-0410-b279-a288a0259246

--- src/odin.iasco/tests/test_dbcalls.py
+++ src/odin.iasco/tests/test_dbcalls.py
@@ -83,27 +83,18 @@ class dbcallsTestCase(mocker.MockerTestCase):
 	
 	"""
 	curs = self.mocker.replace("MySQLdb.cursors.DictCursor")
-        curs(mocker.ARGS,mocker.KWARGS)
         self.mocker.result(None)
 
 	mi = self.mocker.mock()
         mi.execute(mocker.ARGS,mocker.KWARGS)
-        #mi.iter()
-        #self.mocker.result([None].__iter__())
-	self.mocker.result([None])
-        #self.mocker.count(2)
+        iter(mi)
+        self.mocker.result([{'assdate': None}].__iter__())
         mi.close()
         self.mocker.result(None)
-        #self.mocker.count(2)
-        #mi.iter()
-        #self.mocker.result([None].__iter__())
 
-#	def __iter__(self):
-#	    return [None].__iter__()
 	db = self.mocker.mock()
         db.cursor(mocker.ARGS,mocker.KWARGS)
         self.mocker.result(mi)
-        #self.mocker.count(2)
 
         dbconnect = self.mocker.replace("MySQLdb.connect")
         dbconnect(mocker.ARGS,mocker.KWARGS)
@@ -114,12 +105,180 @@ class dbcallsTestCase(mocker.MockerTestCase):
         self.mocker.verify()
 
 
+    def getNewDates_test(self):
+        """Testing getNewDates
+        
+        """
+        curs = self.mocker.replace("MySQLdb.cursors.DictCursor")
+        self.mocker.result(None)
+
+        la = self.mocker.mock()
+        la.execute(mocker.ARGS,mocker.KWARGS)
+        self.mocker.count(2)
+	iter(la)
+        self.mocker.result([(datetime.date(2010,1,1),datetime.date(2010,1,1))].__iter__())
+        iter(la)
+        self.mocker.result([(datetime.date(2010,1,3),datetime.date(2010,1,3))].__iter__())
+
+	la.close()
+        self.mocker.result(None)
+	self.mocker.count(2)
+
+        db = self.mocker.mock()
+        db.cursor(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(la)
+	self.mocker.count(2)
+
+        dbconnect = self.mocker.replace("MySQLdb.connect")
+        dbconnect(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(db)
+
+        self.mocker.replay()
+        self.assertEquals(odin.iasco.db_calls.getNewDates(),[datetime.date(2010, 1, 2)])
+        self.mocker.verify()
+
+    def getWindBool_test(self):
+        """Testing getWindBool
+        
+        """
+        curs = self.mocker.replace("MySQLdb.cursors.DictCursor")
+        self.mocker.result(None)
+
+        cur = self.mocker.mock()
+        cur.execute(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(False)
+        cur.close()
+        self.mocker.result(None)
+
+        db = self.mocker.mock()
+        db.cursor(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(cur)
+
+        dbconnect = self.mocker.replace("MySQLdb.connect")
+        dbconnect(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(db)
+
+        self.mocker.replay()
+        self.assertEquals(odin.iasco.db_calls.getWindBool([],[]),False)
+        self.mocker.verify()
+
+    def getHdfBool_test(self):
+        """Testing getHdfBool
+        
+        """ 
+        curs = self.mocker.replace("MySQLdb.cursors.DictCursor")
+        self.mocker.result(None)
+
+        cur = self.mocker.mock()
+        cur.execute(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(False)
+        cur.close()
+        self.mocker.result(None)
+
+        db = self.mocker.mock()
+        db.cursor(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(cur)
+
+        dbconnect = self.mocker.replace("MySQLdb.connect")
+        dbconnect(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(db)
+
+        self.mocker.replay()
+        self.assertEquals(odin.iasco.db_calls.getHdfBool([],[]),False)
+        self.mocker.verify()
+
+    def getAssimilateBool_test(self):
+        """Testing getAssimilateBool
+        
+        """ 
+        curs = self.mocker.replace("MySQLdb.cursors.DictCursor")
+        self.mocker.result(None)
+            
+        cur = self.mocker.mock()
+        cur.execute(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(False)
+        cur.close()
+        self.mocker.result(None)
+
+        db = self.mocker.mock()
+        db.cursor(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(cur)
+
+        dbconnect = self.mocker.replace("MySQLdb.connect")
+        dbconnect(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(db)
+
+        self.mocker.replay()
+        self.assertEquals(odin.iasco.db_calls.getAssimilateBool([],[]),False)
+        self.mocker.verify()
+
+    def getAssid_test(self):
+        """Testing getAssid
+        
+        """ 
+        curs = self.mocker.replace("MySQLdb.cursors.DictCursor")
+        self.mocker.result(None)
+            
+        a_id = self.mocker.mock()
+        a_id.execute(mocker.ARGS,mocker.KWARGS)
+        iter(a_id)
+        self.mocker.result([{'assid': None}].__iter__())
+
+        a_id.close()
+        self.mocker.result(None)
+
+        db = self.mocker.mock()
+        db.cursor(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(a_id)
+
+        dbconnect = self.mocker.replace("MySQLdb.connect")
+        dbconnect(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(db)
+
+        self.mocker.replay()
+        self.assertEquals(odin.iasco.db_calls.getAssid([],[]),[None])
+        self.mocker.verify()
+
+    def getOrbitInfo_test(self):
+        """Testing getOrbitInfo
+        
+        """
+        curs = self.mocker.replace("MySQLdb.cursors.DictCursor")
+        self.mocker.result(None)
+
+        cur = self.mocker.mock()
+        cur.execute(mocker.ARGS,mocker.KWARGS)
+        iter(cur)
+        self.mocker.result([{'orbit': 0,'start_utc': '2010-1-1','stop_utc': '2010-1-1','id': 0}].__iter__())
+
+        cur.close()
+        self.mocker.result(None)
+
+        db = self.mocker.mock()
+        db.cursor(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(cur)
+
+        dbconnect = self.mocker.replace("MySQLdb.connect")
+        dbconnect(mocker.ARGS,mocker.KWARGS)
+        self.mocker.result(db)
+
+        self.mocker.replay()
+        self.assertEquals(odin.iasco.db_calls.getOrbitInfo([],[],[]),{'start_utc': ['2010-1-1'], 'orbit': [0], 'l1id': [0], 'stop_utc': ['2010-1-1']})
+        self.mocker.verify()
+
+
 def test_suite():
     tests = [
             'w2iasco_test',
             'w2iasco_orbits_test',
-	    #'getStartDate_test',
-	    ]            
+	    'getStartDate_test',
+	    'getNewDates_test',	
+	    'getWindBool_test',
+	    'getHdfBool_test',
+	    'getAssimilateBool_test',
+	    'getAssid_test',
+	    'getOrbitInfo_test',
+ 	    ]            
     return unittest.TestSuite(map(dbcallsTestCase,tests))
 
 if __name__=='__main__':

8bb825be835d100e5023c7758fc5907b515d6374
joakim <joakim@39c22ef0-5628-0410-b279-a288a0259246>
2010-11-19 21:30:30 +0000

    automated tool for building and releasing
    git-svn-id: http://svn.rss.chalmers.se/svn/odinsmr/hermod/trunk@1139 39c22ef0-5628-0410-b279-a288a0259246

---
+++ build_and_release.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+packages=`find src -type f -name setup.py | cut -d/ -f1-2`
+base=`pwd`
+for pkg in $packages
+do
+	echo
+	echo ************* $pkg ************
+	echo
+	cd $pkg
+	../../bin/odinpy setup.py bdist_egg
+	echo *****************
+	echo ** releasing $pkg
+	echo *****************
+	find . -type f -regex .*egg -ctime -1 -exec cp -v \{\} /misc/apps/odinsite \;
+	cd $base
+done

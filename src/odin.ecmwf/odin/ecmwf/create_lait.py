fd942d46b08f762527489343fc2d022e5915f271
joakim <joakim@39c22ef0-5628-0410-b279-a288a0259246>
2012-03-21 16:32:35 +0000

    added framework for Sagi's lait-routines
    git-svn-id: http://svn.rss.chalmers.se/svn/odinsmr/hermod/trunk@1255 39c22ef0-5628-0410-b279-a288a0259246

---
+++ src/odin.ecmwf/odin/ecmwf/create_lait.py
@@ -0,0 +1,37 @@
+
+from odin.config.environment import config,set_hermod_logging
+from odin.config.database import connect
+from os.path import join,basename,split
+from glob import glob
+import logging
+
+def create_insert():
+#    set_hermod_logging()
+    conf = config()
+#    log = logging.getLogger(__name__)
+    basepath = conf.get('ecmwf','basedir')
+
+#    log.info('Scanning database for non existing lait-files')
+    db = connect()
+    cursor1 = db.cursor()
+    cursor2 = db.cursor()
+    cursor1.execute(
+        """
+        select date, hour, ecmwf.filename from ecmwf 
+        left join lait using (date,hour)
+        where     ecmwf.type ='AN' and lait.filename is null
+        """ )
+    for date,hour,filename in cursor1:
+        try:
+            #insert kazutoshi-san's routine
+        except:
+            log.error('some problems creating laitfile from {0}'.format(
+                filename))
+            continue
+        cursor2.execute(
+            '''
+            insert into lait (date,hour,filename) values (%s,%s,%s)
+            ''',(date,hour,laitfile))
+    cursor.close()
+    db.close()
+	

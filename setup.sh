64e7b6af8be8c58862d496292b5645ba9fab689e
Andreas Skyman <andreas.skyman@molflow.com>
2015-11-30 11:54:25 +0100

    Created an embryo of a setup script for installing the hermodules, e.g. in a dockontainer.

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+# Store the root of the Hermod repository:
+HERMOD_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "-------------------------"
+echo " Installing odin.config: "
+echo "-------------------------"
+cd $HERMOD_ROOT/src/odin.config/
+echo " "
+
+echo "------------------------"
+echo " Installing odin.ecmwf: "
+echo "------------------------"
+cd $HERMOD_ROOT/src/odin.ecmwf/
+python setup.py develop
+echo " "
+
+echo "-------------------------"
+echo " Installing odin.hermod: "
+echo "-------------------------"
+cd $HERMOD_ROOT/src/odin.hermod/
+python setup.py develop
+echo " "
+
+echo "------------------------"
+echo " Installing odin.iasco: "
+echo "------------------------"
+cd $HERMOD_ROOT/src/odin.iasco/
+python setup.py develop
+echo " "
+
+cd $HERMOD_ROOT
+
+echo "Done"

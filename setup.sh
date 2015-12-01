a83ba55afa97b226bea5884ef42c89d698fa1330
Andreas Skyman <andreas.skyman@molflow.com>
2015-12-01 09:33:34 +0100

    Added some comments on the intended function of the setup.sh script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
+# This setup file is meant to be run when setting up the Hermod base system
+# Docker container, to automatically install all our Python modules in the
+# container.
+# By default the modules are installed using the "develop" keyword, which means
+# that they are linked dynamically to the source directory and can be updated
+# "on the fly". For release versions the "install" keyword should instead be
+# used, in order to produce a self-contained environment.
 
 # Store the root of the Hermod repository:
 HERMOD_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Install the Python modules:
 echo "-------------------------"
 echo " Installing odin.config: "
 echo "-------------------------"

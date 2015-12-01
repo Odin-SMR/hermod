0415ad175007eaa8a31407481bf165b43a9f85c4
Andreas Skyman <andreas.skyman@molflow.com>
2015-12-01 09:56:34 +0100

    setup.sh now takes optional install argument, which changed the installation mode of the modules.

--- setup.sh
+++ setup.sh
@@ -2,41 +2,53 @@
 # This setup file is meant to be run when setting up the Hermod base system
 # Docker container, to automatically install all our Python modules in the
 # container.
+
+
 # By default the modules are installed using the "develop" keyword, which means
 # that they are linked dynamically to the source directory and can be updated
-# "on the fly". For release versions the "install" keyword should instead be
-# used, in order to produce a self-contained environment.
+# "on the fly". This is useful for development, but for release versions the
+# "install" keyword should instead be used, in order to produce a
+# self-contained environment.
+if [ "$1" = "install" ]; then
+    INSTALL_MODE=install
+else
+    INSTALL_MODE=develop
+fi
+
+echo "Installing python modules in" $INSTALL_MODE "mode"
+
 
 # Store the root of the Hermod repository:
 HERMOD_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 
 # Install the Python modules:
 echo "-------------------------"
 echo " Installing odin.config: "
 echo "-------------------------"
 cd $HERMOD_ROOT/src/odin.config/
-python setup.py develop
+python setup.py $INSTALL_MODE
 echo " "
 
 echo "------------------------"
 echo " Installing odin.ecmwf: "
 echo "------------------------"
 cd $HERMOD_ROOT/src/odin.ecmwf/
-python setup.py develop
+python setup.py $INSTALL_MODE
 echo " "
 
 echo "-------------------------"
 echo " Installing odin.hermod: "
 echo "-------------------------"
 cd $HERMOD_ROOT/src/odin.hermod/
-python setup.py develop
+python setup.py $INSTALL_MODE
 echo " "
 
 echo "------------------------"
 echo " Installing odin.iasco: "
 echo "------------------------"
 cd $HERMOD_ROOT/src/odin.iasco/
-python setup.py develop
+python setup.py $INSTALL_MODE
 echo " "
 
 cd $HERMOD_ROOT

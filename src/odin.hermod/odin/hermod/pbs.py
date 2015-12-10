21143695fdec9a00fd262cc2d977b2d6577ba83e
Andreas Skyman <andreas.skyman@molflow.com>
2015-12-10 10:54:39 +0100

    Cleaned up pbs.py

--- src/odin.hermod/odin/hermod/pbs.py
+++ src/odin.hermod/odin/hermod/pbs.py
@@ -1,5 +1,5 @@
-from subprocess import Popen,PIPE
-from os.path import join,expanduser
+from subprocess import Popen, PIPE
+from os.path import join, expanduser
 from interfaces import IPbs
 from odin.config.environment import config as odin_config
 
@@ -20,30 +20,28 @@ runscript = """
 
 class GEMPbs(IPbs):
 
-    def set_submit_info(self,queue='new'):
+    def set_submit_info(self, queue='new'):
         self.conf = odin_config()
 
         self.info['queue'] = queue
         self.info['torquehost'] = self.conf.get("TORQUE", "torquehost")
-        self.info['jobname'] =  'o%(orbit).4X%(calversion).1f%(fqid).2i%(version)s' % self.info
-        self.info['errfile'] = join(expanduser('~'),'logs',
-                self.info['jobname']+'.err')
-        self.info['outfile'] = join(expanduser('~'),'logs',
-                self.info['jobname']+'.out')
-        self.info['workdir'] = join(expanduser('~'),'Matlab',
-                'Qsmr_%s'%self.info['version'].replace('-','_'))
-
-
+        jobname = ('o%(orbit).4X%(calversion).1f%(fqid).2i%(version)s' %
+                   self.info)
+        self.info['jobname'] = jobname
+        self.info['errfile'] = join(expanduser('~'), 'logs',
+                                    self.info['jobname'] + '.err')
+        self.info['outfile'] = join(expanduser('~'), 'logs',
+                                    self.info['jobname'] + '.out')
+        self.info['workdir'] = join(expanduser('~'), 'Matlab',
+                                    'Qsmr_%s' %
+                                    self.info['version'].replace('-', '_'))
 
     def submit(self):
-        s = Popen([self.conf.get('pbs','batch_command')],stderr=PIPE,
-                stdin=PIPE,stdout=PIPE)
-        s.stdin.write(runscript%self.info)
+        s = Popen([self.conf.get('pbs', 'batch_command')], stderr=PIPE,
+                  stdin=PIPE, stdout=PIPE)
+        s.stdin.write(runscript % self.info)
         s.stdin.close()
         print s.stdout.read()
         print s.stderr.read()
         s.stdout.close()
         s.stderr.close()
-
-
-

727bd2ba4440707d4b42644e478a4c5874c7c325
Andreas Skyman <andreas.skyman@molflow.com>
2016-03-08 15:58:37 +0100

    Replaced erroneous path with absolute dockerpath

--- src/odin.ecmwf/odin/ecmwf/create_odinecmwf.py
+++ src/odin.ecmwf/odin/ecmwf/create_odinecmwf.py
@@ -1,17 +1,18 @@
 from odin.ecmwf.ecmwf_nc import Ecmwf_Grib2
-from odin.config.environment import config,set_hermod_logging
+from odin.config.environment import config, set_hermod_logging
 from odin.config.database import connect
-from os.path import join,basename
+from os.path import join
 from glob import glob
 import logging
+
 
 def create_insert():
     set_hermod_logging()
     conf = config()
     log = logging.getLogger(__name__)
-    basepath = conf.get('ecmwf','spooldir')
+    basepath = conf.get('ecmwf', 'spooldir')
     log.info('Scanning {0} for ECMWF gribfiles'.format(basepath))
-    pattern = join(basepath,'ECMWF_ODIN_*+000H00M')
+    pattern = join(basepath, 'ECMWF_ODIN_*+000H00M')
     file_list = glob(pattern)
     log.info('Found {0} new gribfiles'.format(len(file_list)))
     db = connect()
@@ -27,13 +28,12 @@ def create_insert():
         record['type'] = 'AN'
         record['hour'] = ecmwf.time.hour
         record['filename'] = ecmwf.outfile().split(
-                conf.get('ecmwf','basedir'))[1]
+            conf.get('ecmwf', 'basedir'))[1]
         cursor.execute("""
-                replace ecmwf (date,type,hour,filename) 
+                replace ecmwf (date,type,hour,filename)
                 values (%(date)s, %(type)s, %(hour)s, %(filename)s)
-                """ , (record))
+                """, (record))
         ecmwf.delete()
         log.info('processed file {0}'.format(ecmwf.outfile()))
     cursor.close()
     db.close()
-	
